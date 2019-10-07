ad51525ccfbaae630df3856264673053506d7ae4
Samuel Grave <samuel.grave.silva@outlook.com>
2019-10-07 16:10:32 -0300

    test: creates unit test

--- commands/dice.js
+++ commands/dice.js
@@ -1,9 +1,9 @@
 module.exports.execute = async (client, message, args) => {
-	if(!!args || !args[0] ) {
+	if(!args || !args.length === 0 ) {
 		return await message.channel.send('❌ Please specify the dice size.');
 	}
 
-	if(args[0].isNaN()) {
+	if(isNaN(args[0])) {
 		return await message.channel.send('❌ Please specify a number for the dice size.');
 	}
 	const diceSize = Number.parseInt(args[0], 10);
