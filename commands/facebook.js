25153981d5353ffe81a2984622bd33a315f88fa5
Manon Bertin <56343083+Manon-B@users.noreply.github.com>
2019-10-10 19:12:49 +0200

    Facebook command (#28)
    * Added a facebook command
    
    * Added a test file for the facebook command

---
+++ commands/facebook.js
@@ -0,0 +1,10 @@
+module.exports.execute = async (client, message) => {
+	return await message.channel.send(
+		'KOA Facebook Group : https://www.facebook.com/groups/1895927804047033/',
+	);
+};
+
+module.exports.config = {
+	name: 'facebook',
+	aliases: ['facebook', 'fb'],
+};
