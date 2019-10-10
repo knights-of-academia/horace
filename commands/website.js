d9bf246a2f58f34e3da22594b914b54e173a829e
ziebam <ziebamichal@tutanota.com>
2019-10-10 16:21:02 +0200

    Add a test for the !website command

--- commands/website.js
+++ commands/website.js
@@ -1,8 +1,8 @@
 module.exports.execute = async (client, message) => {
-    return await message.channel.send("⚔️ https://knightsofacademia.org/ ⚔️");
+	return await message.channel.send('⚔️ https://knightsofacademia.org/ ⚔️');
 };
 
 module.exports.config = {
-    name: 'website',
-    aliases: ['website', 'koa']
+	name: 'website',
+	aliases: ['website', 'koa']
 };
