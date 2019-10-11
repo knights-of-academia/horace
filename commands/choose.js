af0939ba78c7e83fc86b8d280a8524bf867d0adf
ziebam <ziebamichal@tutanota.com>
2019-10-11 17:55:48 +0200

    Parse the arguments with a regular expression - Handle weird inputs with multiple commas and spaces better. - Handle empty inputs.

--- commands/choose.js
+++ commands/choose.js
@@ -1,8 +1,13 @@
 module.exports.execute = async (client, message, args) => {
-	args = args.join(' ').split(', ');
+	if (args.length) {
+		args = args.join(' ').split(/,+ */);
 
-	const choiceIndex = Math.floor(Math.random() * args.length);
-	return await message.channel.send('Horace says... ' + args[choiceIndex] + '!');
+
+		const choiceIndex = Math.floor(Math.random() * args.length);
+		return await message.channel.send('Horace says... ' + args[choiceIndex] + '!');
+	}
+
+	return await message.channel.send('Please specify the options I should choose from!\nHint: !choose option1, option2, ..., optionX');
 };
 
 module.exports.config = {
