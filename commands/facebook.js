13199c433ee23c0ed7536a7ea550bc8294da7bea
Zaph <35431127+Zaph-x@users.noreply.github.com>
2019-10-13 09:54:33 +0200

    Update commands/facebook.js
    Fixed Facebook help description
    
    Co-Authored-By: Daniel Moesgaard Andersen <moesgaarda@gmail.com>

--- commands/facebook.js
+++ commands/facebook.js
@@ -7,6 +7,6 @@ module.exports.execute = async (client, message) => {
 module.exports.config = {
 	name: 'facebook',
 	aliases: ['facebook', 'fb'],
-	description: 'I will send you the link to our facebook group.',
+	description: 'I will send you the link to our Facebook group.',
 	usage: ['facebook']
 };
