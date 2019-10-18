e49b71d1abb31d4f63c28ad05e1a3b78cb2bf360
Daniel Moesgaard <moesgaarda@gmail.com>
2019-10-18 08:21:49 +0200

    Added moderation log sending

--- eventActions/snapshotActions.js
+++ eventActions/snapshotActions.js
@@ -1,17 +1,27 @@
 const config = require('../config.json');
+const Discord = require('discord.js');
 
 class snapshotActions {
-    static userPostsImage(client, message) {
-        if (message.channel.id === config.channels.snapshots
+	static async userPostsImage(client, message) {
+		if (message.channel.id === config.channels.snapshots
             && message.attachments.size === 0) {
 
-            // Delete message
+			// Delete message
+			await message.delete();
 
-            // Send warning to author
+			// Send warning to author
+			await message.author.send(`Hey! I noticed you sent a message in <#${config.channels.snapshots}> that wasn't a picture. To help maintain the integrity of the channel, the staff team has made the decision to only allow pictures, captions and reactions there. As a result of this, your message has been removed. Thanks for understanding! 😄`);
 
-            // Send message to moderation log
-        }
-    }
+			const embedMessage = new Discord.RichEmbed()
+				.setColor('#ff0000')
+				.setTitle('🚩 Warning: Snapshots message 🚩')
+				.setDescription(`An unapproved message was sent in <#${config.channels.snapshots}>`)
+				.addField('User', message.author, true)
+				.addField('Message', message.content);
+			// Send message to moderation log
+			client.channels.get(config.channels.moderation).send(embedMessage);
+		}
+	}
 
 }
 
