b879aeef5e13746b9492cc06b8df1b2a99df1542
Daniel Moesgaard <moesgaarda@gmail.com>
2019-10-21 17:39:49 +0200

    Added message deleted moderation

---
+++ eventActions/deleteMessageActions.js
@@ -0,0 +1,21 @@
+const config = require('../config.json');
+const Discord = require('discord.js');
+
+class deleteMessageActions {
+	static async sendMessageToModeration(client, message) {
+		let logs = await message.guild.fetchAuditLogs({ type: 72 });
+		let entry = logs.entries.first();
+
+		let embed = new Discord.RichEmbed()
+			.setTitle('🟡 Warning: Message deleted 🟡')
+			.setColor('#ffae42')
+			.addField('Author', message.author, true)
+			.addField('Channel', message.channel, true)
+			.addField('Message', message.content)
+			.addField('Deleted by', entry.executor);
+		// Send message to moderation log
+		client.channels.get(config.channels.moderation).send(embed);
+	}
+}
+
+module.exports = deleteMessageActions;
