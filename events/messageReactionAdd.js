0969b0b54d1a38eddb7418d3261656aa20dad103
Dendrobyte <markbacon78@gmail.com>
2019-10-20 21:33:48 -0400

    fixing some linting

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,20 +1,14 @@
 const tosActions = require('../eventActions/tosActions');
-<<<<<<< HEAD
 const accountabilityActions = require('../eventActions/accountabilityActions');
-=======
 const pinAction = require('../eventActions/pinAction');
->>>>>>> 126f01c97fb195ada46dfd08916927ac5f72bc22
 
 module.exports = async (client, reaction, user) => {
 	// Handle reaction to the ToS message in ToS channel
 	tosActions.userAcceptsTOS(reaction, user, client);
-<<<<<<< HEAD
 
 	// Handle reaction to a message in accountability cahnnel
 	accountabilityActions.userPinsMessage(reaction, user);
-=======
 	// Handle message pinning in channels
 	pinAction.pinMessage(client, reaction);
->>>>>>> 126f01c97fb195ada46dfd08916927ac5f72bc22
 };
 
