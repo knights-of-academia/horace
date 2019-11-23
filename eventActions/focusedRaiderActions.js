faadc3bf7589e115189550d46e2f9d287ceb6303
Dendrobyte <markbacon78@gmail.com>
2019-11-23 18:06:05 -0500

    Warning when 45 pins, a message when max pins are reached

--- eventActions/focusedRaiderActions.js
+++ eventActions/focusedRaiderActions.js
@@ -4,19 +4,12 @@ class focusedRaiderActions {
 
 	static async giveRole(reaction, user){
 		// Ensure we're in the proper channel and using the proper reaction
-<<<<<<< HEAD
-		if(reaction.message.channel.id == config.channels.chooseroles && reaction._emoji.id == config.emotes.hocReaction) {
-			if(reaction.message.id != config.focusedRaiderMessageId) return;
-			// Create the role variable
-			const role = reaction.message.guild.roles.find(role => role.id === config.roles.focusedraider);
-=======
 		if(reaction.message.channel.id == config.channels.chooseroles && reaction._emoji.name == config.emotes.hocReaction) {
 
 			if(reaction.message.id != config.focusedRaiderMessageId) return;
 
 			// Create the role variable
 			const role = reaction.message.guild.roles.find(role => role.name === config.roles.focusedraider);
->>>>>>> 83df7b1a0087fc84407eb2bed8b473b26b8e2d7d
 
 			// Create a GuildMember object from passed in user, and add role
 			await reaction.message.guild.fetchMember(user).then(activeMember => activeMember.addRole(role));
@@ -25,20 +18,12 @@ class focusedRaiderActions {
 
 	static async removeRole(reaction, user){
 		// Ensure we're in the proper channel and using the proper reaction
-<<<<<<< HEAD
 		if(reaction.message.channel.id == config.channels.chooseroles && reaction._emoji.id == config.emotes.hocReaction) {
-=======
-		if(reaction.message.channel.id == config.channels.chooseroles && reaction._emoji.name == config.emotes.hocReaction) {
->>>>>>> 83df7b1a0087fc84407eb2bed8b473b26b8e2d7d
 
 			if(reaction.message.id != config.focusedRaiderMessageId) return;
 
 			// Create the role variable
-<<<<<<< HEAD
 			const role = reaction.message.guild.roles.find(role => role.id === config.roles.focusedraider);
-=======
-			const role = reaction.message.guild.roles.find(role => role.name === config.roles.focusedraider);
->>>>>>> 83df7b1a0087fc84407eb2bed8b473b26b8e2d7d
 
 			// Create a GuildMember object from passed in user, adn remove role
 			await reaction.message.guild.fetchMember(user).then(activeMember => activeMember.removeRole(role));
@@ -46,8 +31,4 @@ class focusedRaiderActions {
 	}
 }
 
-<<<<<<< HEAD
 module.exports = focusedRaiderActions;
-=======
-module.exports = focusedRaiderActions;
->>>>>>> 83df7b1a0087fc84407eb2bed8b473b26b8e2d7d
--- eventActions/pinAction.js
+++ eventActions/pinAction.js
@@ -3,14 +3,31 @@ const config = require('../config.json');
 class pinAction {
 
 	static async pinMessage(client, reaction) {
-		if (reaction.message.channel.id === config.channels.accountability) return;
+		// Check for max pins regardless of channel
+		const currentChannel = reaction.message.channel;
+		let numOfPins = 0;
+		currentChannel.fetchPinnedMessages().then(messages =>
+			numOfPins = messages.size);
+		if(numOfPins == 50){
+			currentChannel.send('**Uh oh!** This channel has reached its pin limit.');
+			return;
+		}
+		else if(numOfPins >= 45){
+			currentChannel.send('**Heads up!** This channel has almost at its pin limit! There are currently ' + numOfPins + ' pinned messages.');
+			return;
+		}
+
+		// If it's accountability station, manage that
+		if (currentChannel.id === config.channels.accountability) return;
+
+		// Server-wide pin feature
 		const pinLimit = config.pinLimit;
 		if (reaction._emoji.name === config.emotes.pinMessage && reaction.count >= pinLimit) {
 			return reaction.message.pin();
 		} else if (reaction._emoji.name === config.emotes.pinMessage &&
 			reaction.message.pinned &&
 			reaction.message < pinLimit) {
-			reaction.message.channel.send('Your message was successfully unpinned');
+			currentChannel.send('Your message was successfully unpinned');
 			return reaction.message.unpin();
 		}
 	}
