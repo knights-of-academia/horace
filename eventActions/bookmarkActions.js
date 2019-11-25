acd099d8edcf3895dca284e67e87d64b3b0ad8d2
Colin Pokowitz <cp@cpdev.me>
2019-11-24 21:30:37 -0600

    Few visual fixes to bookmarks!

--- eventActions/bookmarkActions.js
+++ eventActions/bookmarkActions.js
@@ -9,11 +9,15 @@ class bookmarkActions {
 			workingMessage.guild.fetchMember(workingMessage.author.id).then(guildMember => {
 				const bookmarkEmbed = new Discord.RichEmbed()
 					.setColor('#0F9BF1')
-					.setTitle('Knights of Academia Bookmark')
+					.setTitle(`${config.emotes.cotwVow} Knights of Academia Bookmark ${config.emotes.cotwVow}`)
 					.setDescription('You asked to bookmark this post from the Knights of Academia server.')
 					.addField('Channel', workingMessage.channel)
                     .addField('Author', workingMessage.author)
-                    .addField('Message', workingMessage.content);
+                    const messageChunks = workingMessage.content.match(/[\s\S]{1,1024}/g);
+
+			        for (let chunk of messageChunks) {
+				        bookmarkEmbed.addField('Message', chunk);
+			        }
 
 			        guildMember.send(bookmarkEmbed);
 			});
