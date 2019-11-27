e68709609342e77f3d439de1c180957948429170
Colin <16247799+cosmiccdev@users.noreply.github.com>
2019-11-27 01:33:28 -0600

    Bookmark feature (#98)
    * Base Bookmark Feature
    
    * Working code, sort of. Bugs
    
    * Forgot about modules.export...
    
    * Attempted fixes
    
    Closest so far, but still not working. `message not defined` error
    
    * Message still shows as undefined
    
    * Try 2, adding arg to raw.js
    
    Still showing message as undefined
    
    * El fixes
    
    * Removed testing comments
    
    * Few visual fixes to bookmarks!
    
    * Add info term abt cosmicdev
    
    * Remove old testing additions
    
    * Fix linting issues
    
    * Post-review fixes
    
    * Switch cotwVow emote to local variable

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -2,6 +2,7 @@ const tosActions = require('../eventActions/tosActions');
 const accountabilityActions = require('../eventActions/accountabilityActions');
 const pinAction = require('../eventActions/pinAction');
 const focusedRaiderActions = require('../eventActions/focusedRaiderActions');
+const bookmarkActions = require ('../eventActions/bookmarkActions');
 
 module.exports = async (client, reaction, user) => {
 	// Handle reaction to the ToS message in ToS channel
@@ -9,11 +10,13 @@ module.exports = async (client, reaction, user) => {
 
 	// Handle reaction to a message in accountability channel
 	accountabilityActions.userPinsMessage(reaction, user);
+
 	// Handle message pinning in channels
 	pinAction.pinMessage(client, reaction);
 
 	// Add focused raider role
 	focusedRaiderActions.giveRole(reaction, user);
 
+	// Bookmark messages in DMs
+	bookmarkActions.bookmarkMessage(client, reaction);
 };
-
