b44cfe08da85f1122febf1210848d8700e846c2f
Colin Pokowitz <cp@cpdev.me>
2019-11-27 14:44:05 -0600

    Horace now sends the bookmark to the reactor
    Not the message author....

--- eventActions/bookmarkActions.js
+++ eventActions/bookmarkActions.js
@@ -3,25 +3,24 @@ const Discord = require('discord.js');
 
 class bookmarkActions {
 
-	static async bookmarkMessage(client, reaction) {
+	static async bookmarkMessage(client, user, reaction) {
 		if (reaction._emoji.name === config.emotes.bookmark) {
 			const workingMessage = reaction.message;
 			const swordsEmote = '⚔';
-			workingMessage.guild.fetchMember(workingMessage.author.id).then(guildMember => {
-				const bookmarkEmbed = new Discord.RichEmbed()
-					.setColor('#0F9BF1')
-					.setTitle(`${swordsEmote} Knights of Academia Bookmark ${swordsEmote}`)
-					.setDescription('You asked to bookmark this post from the Knights of Academia server.')
-					.addField('Channel', workingMessage.channel)
-					.addField('Author', workingMessage.author);
-				const messageChunks = workingMessage.content.match(/[\s\S]{1,1024}/g);
+			console.log(user);
+			const bookmarkEmbed = new Discord.RichEmbed()
+				.setColor('#0F9BF1')
+				.setTitle(`${swordsEmote} Knights of Academia Bookmark ${swordsEmote}`)
+				.setDescription('You asked to bookmark this post from the Knights of Academia server.')
+				.addField('Channel', workingMessage.channel)
+				.addField('Author', workingMessage.author);
+			const messageChunks = workingMessage.content.match(/[\s\S]{1,1024}/g);
 
-				for (const chunk of messageChunks) {
-					bookmarkEmbed.addField('Message', chunk);
-				}
+			for (const chunk of messageChunks) {
+				bookmarkEmbed.addField('Message', chunk);
+			}
 
-				guildMember.send(bookmarkEmbed);
-			});
+			user.send(bookmarkEmbed);
 		}
 	}
 }
