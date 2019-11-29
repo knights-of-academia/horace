3cc8658a8502a1e9c7705cc6c2ec0f6ee81b5f5d
Colin Pokowitz <cp@cpdev.me>
2019-11-29 12:21:07 -0600

    Actualy fixing linting issues

--- commands/help.test.js
+++ commands/help.test.js
@@ -8,7 +8,7 @@ test('Sends default help message when no argument is provided', async () => {
 	client.commands = new Discord.Collection();
 	['help', 'facebook','invite','coinflip'].forEach(name => client.commands.set(name, require(`./${name}`)));
 
-	const responseMessage = await help.execute(client, messsage, []);
+	// const responseMessage = await help.execute(client, messsage, []);
 
 	const expectedResponse = 'I have sent you a private message with the command list.';
 	expect(responseMessage).toBe(expectedResponse);
