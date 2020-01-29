ca4da7fd7f18340d784e92439c49627dafb70a7a
Zaph-x <mikkel@luceartfoto.dk>
2020-01-29 22:15:17 +0100

    Made initial Afk table

---
+++ databaseFiles/afkTable.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+const connect = require('./connect.js');
+
+const sequelize = connect.sequelize;
+
+module.exports = sequelize.define('Afk', {
+    message: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    user: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
