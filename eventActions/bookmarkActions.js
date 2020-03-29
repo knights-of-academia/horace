e784cdca86a45f88595cccf7c173443e3c1b053f
Dendrobyte <markbacon78@gmail.com>
2020-03-28 20:11:06 -0400

    Updated so that attachment link is added (image can't be recreated and sent)

--- eventActions/bookmarkActions.js
+++ eventActions/bookmarkActions.js
@@ -11,14 +11,17 @@ class bookmarkActions {
 				.setColor('#0F9BF1')
 				.setTitle(`${swordsEmote} Knights of Academia Bookmark ${swordsEmote}`)
 				.setDescription('You asked to bookmark this post from the Knights of Academia server.')
-				.addField('Full Message', workingMessage)
 				.addField('From', workingMessage.author, true)
 				.addField('Link to Message', `[Jump to Message](${workingMessage.url})`, true)
 				.addField('Channel', workingMessage.channel);
 			const messageChunks = workingMessage.content.match(/[\s\S]{1,1024}/g);
 
 			for (const chunk of messageChunks) {
-				bookmarkEmbed.addField('Message', chunk);
+				bookmarkEmbed.addField('Full Message', chunk);
+				if(workingMessage.attachments.array().length > 0){
+					const attchmnt = workingMessage.attachments.array()[0].url;
+					bookmarkEmbed.addField('Attachment', attchmnt)
+				}
 			}
 
 			user.send(bookmarkEmbed);
