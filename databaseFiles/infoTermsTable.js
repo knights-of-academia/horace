6bcd90871076ead9f45e92ca84613a0bd52df851
RaniOfTheRain <chloecampbell911@gmail.com>
2020-04-03 18:10:47 +0100

    Add files via upload

---
+++ databaseFiles/infoTermsTable.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+const connect = require('./connect.js');
+
+const sequelize = connect.sequelize;
+
+// A table representing info terms about key aspects of KOA
+module.exports = sequelize.define('InfoTerms', {
+    //Attributes of table 'infoterms'
+    term: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
