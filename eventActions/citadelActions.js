301e10cbd3a5e67730a4d5e0533fcf93a50ec0a3
Caio F <caio.fleury.r@gmail.com>
2020-04-09 00:07:40 +0300

    Fixed eslint issue on the regex for citadel actions

--- eventActions/citadelActions.js
+++ eventActions/citadelActions.js
@@ -3,8 +3,8 @@ const config = require('../config.json');
 class citadelActions {
 	static async greetMorningOrNight(client, message) {
 		// Handle good morning and goodnight
-		let reg_morning = new RegExp("mo*rning (koa\W*|knights\W*|friends\W*|everyone\W*)");
-		let reg_night = new RegExp("ni*ght (koa\W*|knights\W*|friends\W*|everyone\W*)");
+		let reg_morning = new RegExp('mo*rning (koa[^a-z]*|knights[^a-z]*|friends[^a-z]*|everyone[^a-z]*)$');
+		let reg_night = new RegExp('ni*ght (koa[^a-z]*|knights[^a-z]*|friends[^a-z]*|everyone[^a-z]*)$');
 		if (message.channel.id === config.channels.citadel) {
 			if (reg_morning.test(message.content.toLowerCase())) {
 				return await message.react(config.emotes.goodmorning);
