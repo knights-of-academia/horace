a190d7616bd92b94c5346fde75e6f0eabb9d6bd3
Colin <colin@cpdev.me>
2020-07-28 20:01:47 -0500

    Linting issue fixes

--- commands/contribute.js
+++ commands/contribute.js
@@ -1,9 +1,7 @@
 module.exports.execute = async (client, message) => {
 	return await message.channel.send(`To submit a feature, simply click on the link below and submit a request. Our Engineering Team will sort through these requests and once approved, build them in the following Horace update/patch. If you know Javascript or want to learn, feel free to submit a Pull Request!
-
-https://github.com/Knights-Of-Academia/horace/issues
-    
-*Note: you do need to sign up for a Github account to do this*`);
+	https://github.com/Knights-Of-Academia/horace/issues
+	*Note: you do need to sign up for a Github account to do this*`);
 };
 
 module.exports.config = {
--- commands/contribute.test.js
+++ commands/contribute.test.js
@@ -11,10 +11,8 @@ test('Sends correct message to channel', async () => {
 
 	// Assert
 	const expectedResponse = `To submit a feature, simply click on the link below and submit a request. Our Engineering Team will sort through these requests and once approved, build them in the following Horace update/patch. If you know Javascript or want to learn, feel free to submit a Pull Request!
-
-    https://github.com/Knights-Of-Academia/horace/issues
-        
-    *Note: you do need to sign up for a Github account to do this*`;
+	https://github.com/Knights-Of-Academia/horace/issues
+	*Note: you do need to sign up for a Github account to do this*`;
 
 	expect(responseMessage).toBe(expectedResponse);
 });
