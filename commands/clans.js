fc111a79581315f083e166643681551e0691b65b
Anna Hu <lanqueh42@gmail.com>
2020-09-11 22:59:43 +0100

    display the leader's disord username if he/she is in db

--- commands/clans.js
+++ commands/clans.js
@@ -2,7 +2,8 @@
 const clans = require('../data/clan-data');
 const Discord = require('discord.js');
 const Habitica = require('habitica');
-
+const Habiticas = require('../databaseFiles/habiticaTable.js');
+const habHelper = require('../utils/habiticaHelper')
 const config = require('../config.json');
 let api;
 
@@ -11,18 +12,18 @@ module.exports.execute = async (client, message) => {
 	await Promise.all(clans.map(async clan => {
 	//await clans.reduce(async (memo, clan) => { // ensure they return in the order of clan-data
 		//await memo;
-		api = new Habitica({
+		let api = new Habitica({
 			id: clan.id,
 			apiToken: clan.apiToken
 		});
-		clanInfo = await api.get(`/groups/party`).then(res => {return res.data;})
+		let clanInfo = await api.get(`/groups/party`).then(res => {return res.data;})
 			.catch(err => {console.log(`There has been a problem in fetching clan ${clan.fullName}: ${err}`);});
-		// TODO: read in leader's discord id using database, if not exist then use habitica userName
-		clanLeader = clanInfo.leader.profile.name;//id;
-		clanLeaderDescriptor = '0000';
-		memberCount = clanInfo.memberCount;
-		response += `🔸 **${clanInfo.name}**: ${clan.description} by **${clanLeader}**#${clanLeaderDescriptor}\n`
-		response += `        Current members: ${memberCount}/30\n`
+		// find the leader's discord id using database, if not exist then use habitica userName
+		let clanLeader = await habHelper.findDiscordUser(client, clanInfo.leader.id);// TODO: deal with the case that there are two user having same habitica ID
+		let leaderInfo = clanLeader.length==1?`**${clanLeader[0].username}**#${clanLeader[0].discriminator}`:`**@${clanInfo.leader.profile.name}**`
+		let memberCount = clanInfo.memberCount;
+		response += `🔸 **${clanInfo.name}**: ${clan.description} by ${leaderInfo}\n`
+		response += `             Current members: ${memberCount}/30\n`
 	}));
 	//}, undefined);
 	return await message.channel.send(response);
