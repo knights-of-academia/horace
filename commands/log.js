5bfc0c63f45671e5f0fc6d43bc318d3bdc04afae
Anna Hu <lanqueh42@gmail.com>
2020-09-15 22:52:40 +0100

    rename module for counting vovs

---
+++ commands/log.js
@@ -0,0 +1,80 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const config = require('../config.json');
+const helper = require('../utils/utils');
+
+let prefix;
+if (fs.existsSync('../config.json')) {
+	prefix = require('../config.json');
+} else {
+	prefix = '!';
+}
+
+module.exports.execute = async (client, message, args) => {
+    if (message.channel.id === config.channels.cotw) {
+        checker = str => str.toLowerCase().includes('i vow to');
+    } else{
+        return message.channel.send(`${prefix}log is only avaliable in <#${config.channels.cotw}>`)
+    }
+
+    if (!args || args.length === 0) {// default case: no args: return the author's record 
+        logs = await helper.fetchMessageWith(checker, message, message.author.id, config.channels.cotw);
+
+        return await message.channel.send(`Hey, ${message.author.username}, you have vowed in ${logs.length} challenge of the week.
+            If you want the details of your vows, send ${prefix}cotw log details`);
+
+    } else if (args.length === 1 && args[0].toLowerCase() === 'details'){// send a detailed log of the messages
+        logs = await helper.fetchMessageWith(checker, message, message.author.id, config.channels.cotw);
+        if (logs.length != 0) {// have vows: send a private message of all vows
+            // TODO: ensure the message does not exceed character limit
+            let embedArray = helper.splitArray(logs.sort((a,b)=>a.createdAt-b.createdAt),25);
+            embedArray.forEach(arr=>{
+                let vowLogMessage = new Discord.RichEmbed()
+                    .setColor('#442477') // color of habitical logo
+                    .setTitle('Your vows')
+                    .setDescription(`Below are the vows you made for COTW.`);
+                
+                    arr.forEach(log => {// v12: each.log=>
+                        vowLogMessage.addField(`**In ${log.createdAt.toDateString()}**`, `${log.content}`);
+                    });
+                try {
+                    message.author.send(vowLogMessage);
+                }
+                catch(err) {
+                    console.log(err);
+                }
+
+            });
+
+            return await message.channel.send('I have sent you private message(s) with all the vows you made.')
+                .catch(err => console.error(err) );
+        } else {// no log:
+            return await message.channel.send('Sorry, I cannot find any of your vows.').catch(err => {
+                console.error(err);
+            });
+        }
+
+    }else {// case that mentions one or more users
+        let mentionedUsers = message.mentions.users;
+        if (mentionedUsers.size == 0){
+            return await message.channel.send(`I don't know what you means. The allowed command for cowtlog are ${prefix}cowtlog, ${prefix}cowtlog details or ${prefix} @<username>`);
+        } else{// use trycatch?
+            mentionedUsers.forEach(user=>{
+                helper.fetchMessageWith(checker, message, user.id, config.channels.cotw).then(logs =>{
+                    message.channel.send(`Hey, ${message.author.username}, ${user.username} have vowed in ${logs.length} challenge of the week.`);
+                });
+            });
+            return;
+        }
+
+    }
+	
+};
+
+module.exports.config = {
+	name: 'log',
+	aliases: ['log'],
+	description: 'I will tell you how many times a user participates in the challenge of the week.',
+	usage: ['log', 'log @user', 'log details'],
+};
+
--- utils/utils.js
+++ utils/utils.js
@@ -1,34 +1,35 @@
-
+module.exports = {
 
 // returns a collection of messages with content that satisfies the checker from author in channelID
 
-async function fetchMessageWith(checker, message, authorID, channelID) {
-    //v12: let searchChannel = message.guild.channels.cache.get(channelID);
-    let searchChannel = message.guild.channels.get(channelID);
-    // function to check if a string contains any of the searchTerms
+    fetchMessageWith: async function(checker, message, authorID, channelID) {
+        //v12: let searchChannel = message.guild.channels.cache.get(channelID);
+        let searchChannel = message.guild.channels.get(channelID);
+        // function to check if a string contains any of the searchTerms
 
-    let fetchOK = true;
-    let messageArr =[];
-    do {
-        messages = await searchChannel.fetchMessages({ limit: 100 });
-        try {
-            filtered = messages.filter(m => m.author.id === authorID && checker(m.content) && !m.deleted);
-            messageArr = messageArr.concat(Array.from(filtered.values()));
-        } catch (err) {
-            fetchOK = false;
+        let fetchOK = true;
+        let messageArr =[];
+        do {
+            messages = await searchChannel.fetchMessages({ limit: 100 });
+            try {
+                filtered = messages.filter(m => m.author.id === authorID && checker(m.content) && !m.deleted);
+                messageArr = messageArr.concat(Array.from(filtered.values()));
+            } catch (err) {
+                fetchOK = false;
+            }
+
+        } while (messages.size === 100 && fetchOK); // fetch until reaching the beginning of the channel. or have error with fetching
+        return messageArr;
+
+    },
+
+    // return an array of array each with length len
+
+    splitArray: function(map,len) {
+        let arrayOfMaps = [];
+        for (var i=0; i<map.length; i+=len) {
+            arrayOfMaps.push(map.slice(i,i+len));
         }
-
-    } while (messages.size === 100 && fetchOK); // fetch until reaching the beginning of the channel. or have error with fetching
-    return messageArr;
-
-}
-
-// return an array of array each with length len
-
-function splitArray(map,len) {
-    let arrayOfMaps = [];
-    for (var i=0; i<map.length; i+=len) {
-        arrayOfMaps.push(map.slice(i,i+len));
-    }
-    return arrayOfMaps;
+        return arrayOfMaps;
+    },
 }
