3a7d21f4b27ec99d0d7a91c3d0e460ac9b4c6c5d
Anna Hu <lanqueh42@gmail.com>
2020-09-23 22:31:50 +0100

    add sample clan auth data file

---
+++ data/clan-data-auth-sample.js
@@ -0,0 +1,10 @@
+module.exports = [
+	{
+		fullName: 'Clan Name',
+		description:'A short description',
+		clanId: '0000-0000-0000',
+		authId:'0000-0000-0000',// id of a user in the clan, used to retrive clan infomation
+		apiToken:'0000-0000-0000',
+		formUrl: 'https://link-to-application-form/'
+	}
+];
