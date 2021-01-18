02efbc8adb1f9fbc23933d7a2c6bcf06261cf5fd
Catalin Popescu <catalinp86@gmail.com>
2021-01-17 20:51:42 -0500

    fix linting errors in profanityActions

--- eventActions/profanityActions.js
+++ eventActions/profanityActions.js
@@ -1,37 +1,37 @@
-const config = require('../config.json');
 const Discord = require('discord.js');
 const db = require('quick.db');
-class profanityActions {
+const config = require('../config.json');
 
-	static async checkForProfanity(client, message) {
-		const bannedWords = await db.get(`bannedWords-${message.guild.id}`);
-		
-		// check that moderation channelID is valid before attempting profanity check
-		if (client.channels.cache.get(config.channels.moderation) === undefined) {
-			console.log("Error! Moderation Channel ID in Config in likely invalid. Please verify!");
-			return;
-		}
-		else if (!bannedWords) { // ensure bannedWords is populated
-			console.log("Error: No banned words found in database.");
-			return;
-		}
-		else if (bannedWords.some((word) => message.content.includes(word))) {
-			const embedMessage = new Discord.MessageEmbed()
-				.setColor('#ff0000')
-				.setTitle('🚩 Warning: Profanity detected 🚩')
-				.setDescription(`Profanity detected in ${message.channel}`)
-				.addField('User', message.author, true)
-				.addField('Link', `[Go to message](${message.url})`, true)
-				.addField('Message', `**${message.content}**`, true)
-				.setFooter(
-					message.author.username + '#' + message.author.discriminator,
-					message.author.avatarURL
-				);
-			return client.channels.cache
-				.get(config.channels.moderation)
-				.send(embedMessage);
-		}
-	}
+class profanityActions {
+  static async checkForProfanity(client, message) {
+    const bannedWords = await db.get(`bannedWords-${message.guild.id}`);
+
+    // check that moderation channelID is valid before attempting profanity check
+    if (client.channels.cache.get(config.channels.moderation) === undefined) {
+      console.log('Error! Moderation Channel ID in Config in likely invalid. Please verify!');
+    }
+    else if (!bannedWords) { // ensure bannedWords is populated
+      console.log('Error: No banned words found in database.');
+    }
+
+    if (bannedWords.some((word) => message.content.includes(word))) {
+      const embedMessage = new Discord.MessageEmbed()
+        .setColor('#ff0000')
+        .setTitle('🚩 Warning: Profanity detected 🚩')
+        .setDescription(`Profanity detected in ${message.channel}`)
+        .addField('User', message.author, true)
+        .addField('Link', `[Go to message](${message.url})`, true)
+        .addField('Message', `**${message.content}**`, true)
+        .setFooter(
+          `${message.author.username}#${message.author.discriminator}`,
+          message.author.avatarURL,
+        );
+      return client.channels.cache
+        .get(config.channels.moderation)
+        .send(embedMessage);
+    }
+    return null;
+  }
 }
 
 module.exports = profanityActions;
