4fd1dbcac466bff13bf327e3d64d9b2775d8506c
Catalin Popescu <catalinp86@gmail.com>
2021-02-08 16:32:49 -0500

    Revert back to fix difference value

--- commands/remind.js
+++ commands/remind.js
@@ -351,8 +351,8 @@ async function scanForReminders(client) {
 	if (reminders) {
 		let difference;
 		reminders.forEach(async reminder => {
-			difference = reminder.dataValues.whenToRemind - currentDate;
-			if (difference > config.reminderScanInterval) {
+			difference = currentDate - reminder.dataValues.whenToRemind;
+			if (difference > (-1)*config.reminderScanInterval) {
 				remind(client, currentDate, reminder);
 			}
 		});
