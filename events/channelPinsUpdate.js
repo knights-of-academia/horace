36b6aec0133cf00a66d0b50bbfa4ed2d2132ef63
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2021-03-07 14:55:18 -0700

    Update channel pin limit checks for v12 (#225)
    * updated channel pin limit checks for v12
    
    * remove debug line

--- events/channelPinsUpdate.js
+++ events/channelPinsUpdate.js
@@ -1,9 +1,8 @@
 module.exports = async (client, channel) => {
 	// Check for max pins regardless of channel
 	const currentChannel = client.channels.cache.get(channel.id);
-	currentChannel.fetchPinnedMessages().then(messages => {
+	currentChannel.messages.fetchPinned().then(messages => {
 		const numOfPins = messages.size;
-		// currentChannel.send('**Listen!** Currently at ' + numOfPins + ' pins');
 		if(numOfPins === 50){
 			currentChannel.send('**Uh oh!** This channel has reached its pin limit. Contact a Helper to purge the list.');
 			return;
