e661df0b7a4f1b669a689d8b14e0c2ff0cdd85b7
mukilan <mukilnikil7@gmail.com>
2021-04-21 11:36:24 +0530

    Added article command

---
+++ commands/article.js
@@ -0,0 +1,12 @@
+module.exports.execute = async (client, message) => {
+	return await message.channel.send(
+		'KOA Articles Page : https://knightsofacademia.org/category/articles/',
+	);
+};
+
+module.exports.config = {
+	name: 'article',
+	aliases: ['facebook'],
+	description: 'I will send you the link to our Articles page.',
+	usage: ['article']
+};
---
+++ commands/article.test.js
@@ -0,0 +1,18 @@
+const article = require('./article.js');
+const messageStub = require('../stub/messageStub.js');
+
+test("Sends Correct article message to the channel",async()=>{
+    //Arange 
+    const message = new messageStub();
+
+    //Act
+    const responseMessage = await article.execute(null,message);
+
+    //Assert
+    // Assert
+	const expectedResponse = 'KOA Articles Page : https://knightsofacademia.org/category/articles/';
+	expect(responseMessage).toBe(expectedResponse);
+
+
+
+});
