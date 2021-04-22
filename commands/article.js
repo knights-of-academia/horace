fbc02c80e598ce2f64f33e00f36976e5579ebc3e
mukilan <mukilnikil7@gmail.com>
2021-04-22 13:35:35 +0530

    Grammar/Capitalization fix

--- commands/article.js
+++ commands/article.js
@@ -1,20 +1,19 @@
 const config = require('../config.json');
 const Parser = require('rss-parser');
 const parser = new Parser();
-const random = require('random');
 
 
 
 module.exports.execute = async (client, message, args) => {
 	const feed = await parser.parseURL('https://knightsofacademia.org/category/articles/feed');
-	const r = random.int(0 , feed.items.length);
-	let response = `Here is a article i found called "${(feed.items[r].title)}" here is the link to it ${feed.items[r].link}`;
+	const r = Math.floor(Math.random() * (feed.items.length + 1))
+	let response = `Here's a random article from KOA titled "${(feed.items[r].title)}": ${feed.items[r].link}`;
 	if(args[0]){
 		if(args[0].toLowerCase()==='home'){
-			response = "This is home page of the articles https://kingsofacademacia.org/category/articles";
+			response = "KOA Article Archive: https://knightsofacademia.org/category/articles";
 		}
 		else if(args[0].toLowerCase() === 'latest'){
-			response = `Here is the latest article i found called "${(feed.items[0].title)}" here is the link to it ${feed.items[0].link}` 
+			response = `The latest article from KOA is called "${(feed.items[0].title)}" and can be found here: ${feed.items[0].link}.` 
 		}
 	}
 	return await message.channel.send(response);
