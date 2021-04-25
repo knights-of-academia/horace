88748c00fc00370a9aa56349d4a978095e2bb895
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2021-04-25 12:40:59 -0700

    Updated test file for new raid command

--- commands/raid.test.js
+++ commands/raid.test.js
@@ -1,15 +1,34 @@
 const raid = require('./raid.js');
 const messageStub = require('../stub/messageStub');
+const config = require('../config.json');
 
 
-test('Sends correct raid message to channel', async () => {
-	// Arrange
+test('Sends correct raid message to normal channels', async () => {
 	const message = new messageStub();
+	message.channel.id = config.channels.raidroom;
 
-	// Act
 	const responseMessage = await raid.execute(null, message);
 
-	// Assert
 	const expectedResponse = '**RAAAAAAAAAAAAAAAAAAAID⚔** with ' + message.author + '\nhttps://cuckoo.team/koa';
+	expect(responseMessage).toBe(expectedResponse);
+});
+
+test('Sends correct raid message to raidroom50', async () => {
+	const message = new messageStub();
+	message.channel.id = config.channels.raidroom50;
+
+	const responseMessage = await raid.execute(null, message);
+
+	const expectedResponse = '**RAAAAAAAAAAAAAAAAAAAID⚔** with ' + message.author + '\nhttps://cuckoo.team/raid50';
+	expect(responseMessage).toBe(expectedResponse);
+});
+
+test('Sends correct raid message to raidroom90', async () => {
+	const message = new messageStub();
+	message.channel.id = config.channels.raidroom90;
+
+	const responseMessage = await raid.execute(null, message);
+
+	const expectedResponse = '**RAAAAAAAAAAAAAAAAAAAID⚔** with ' + message.author + '\nhttps://cuckoo.team/raid90';
 	expect(responseMessage).toBe(expectedResponse);
 });
--- stub/messageStub.js
+++ stub/messageStub.js
@@ -1,10 +1,13 @@
 module.exports = class messageStub {
 	constructor() {
 		this.channel = {
-			send: async function (message) { return Promise.resolve(message); }
+			send: async function (message) { return Promise.resolve(message); },
+			id: '',
 		};
 		this.author = {
-			send: async function (message) { return Promise.resolve(message); }
+			send: async function (message) { return Promise.resolve(message); },
+			id: '',
+			username: ''
 		};
 	}
 };
