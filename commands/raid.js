44aa36974ddf7e3772b72bd67dcbd3b202b416e5
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2021-04-25 00:43:41 -0700

    Update !raid command for new raidrooms

--- commands/raid.js
+++ commands/raid.js
@@ -1,7 +1,17 @@
-﻿module.exports.execute = async (client, message) => {
-	return await message.channel.send(
-		'**RAAAAAAAAAAAAAAAAAAAID⚔** with ' + `<@${message.author.id}>` + '\nhttps://cuckoo.team/koa',
-	);
+﻿let config = require('../config.json');
+
+module.exports.execute = async (client, message) => {
+	let newMessage;
+	if (message.channel == config.channels.raidroom50) {
+		newMessage = '**RAAAAAAAAAAAAAAAAAAAID⚔** with ' + `<@${message.author.id}>` + '\nhttps://cuckoo.team/raid50';
+	}
+	else if (message.channel == config.channels.raidroom90) {
+		newMessage = '**RAAAAAAAAAAAAAAAAAAAID⚔** with ' + `<@${message.author.id}>` + '\nhttps://cuckoo.team/raid90';
+	}
+	else {
+		newMessage = '**RAAAAAAAAAAAAAAAAAAAID⚔** with ' + `<@${message.author.id}>` + '\nhttps://cuckoo.team/koa';
+	}
+	return await message.channel.send(newMessage);
 };
 
 module.exports.config = {
