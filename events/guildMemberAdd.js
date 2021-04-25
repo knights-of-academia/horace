5739bcd90ab5ff127252226dd29bc6558173481d
mukilan <68199503+mukkuzzz@users.noreply.github.com>
2021-04-25 18:36:56 +0530

    Added the function to the event

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,4 +3,5 @@ const tosReminderAction = require("../eventActions/tosReminderAction");
 
 module.exports = (client, member) => {
 	member.roles.add(member.guild.roles.cache.find(role => role.id === config.roles.initiate));
+	tosReminderAction.addToDatabase(member,new Date());
 };
