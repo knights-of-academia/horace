76566c2531069697225f602d6e06c7446e359b1b
Debashis Biswas <debashis.biswas@asu.edu>
2021-05-13 15:25:04 -0700

    change order of calls in ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,10 +4,10 @@ const { sequelize } = require('../databaseFiles/connect');
 const remind = require('../commands/remind');
 
 module.exports = async (client) => {
-	chainMessageAction.loadChainMessageTracker(client);
-	console.log(`Running on ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers.`);
 	await sequelize.sync();
+	chainMessageAction.loadChainMessageTracker(client);
 	remind.catchUp(client);
 	setInterval(remind.scanForReminders, config.reminderScanInterval, client);
 	client.user.setActivity('Ready to !help');
+	console.log(`Running on ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers.`);
 };
