206a923c76694e312f7bb2745aa8af0d71dab7d8
Debashis Biswas <debashis.biswas@asu.edu>
2021-05-13 14:55:53 -0700

    fixed article tests

--- commands/article.test.js
+++ commands/article.test.js
@@ -3,22 +3,21 @@ const messageStub = require('../stub/messageStub.js');
 
 test('Sends correct response with no arguments', async() => {
 	const message = new messageStub();
-	const responseMessage = await article.execute(null, message);
+	const responseMessage = await article.execute(null, message, []);
 	const expectedStart = 'Here\'s a random article from KOA titled';
-	expect(responseMessage.startsWith(expectedStart)).toBeTruthy();
+	expect(responseMessage).toMatch(new RegExp(`^${expectedStart}`));
 });
 
 test('Sends correct response when home page is requested', async() => {
 	const message = new messageStub();
-	const responseMessage = await article.execute(null, message, 'home');
+	const responseMessage = await article.execute(null, message, ['home']);
 	const expectedStart = 'KOA Article Archive:';
-	console.log(responseMessage);
-	expect(responseMessage.startsWith(expectedStart)).toBeTruthy();
+	expect(responseMessage).toMatch(new RegExp(`^${expectedStart}`));
 });
 
 test('Sends correct response when latest article is requested', async() => {
 	const message = new messageStub();
-	const responseMessage = await article.execute(null, message, 'latest');
+	const responseMessage = await article.execute(null, message, ['latest']);
 	const expectedStart = 'The latest article from KOA is';
-	expect(responseMessage.startsWith(expectedStart)).toBeTruthy();
+	expect(responseMessage).toMatch(new RegExp(`^${expectedStart}`));
 });
