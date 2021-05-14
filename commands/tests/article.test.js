c151f9cbdd4e4676aeb5c2f6bce11f19810142d4
Debashis Biswas <debashis.biswas@asu.edu>
2021-05-14 15:58:22 -0700

    mock article test, update help test

--- commands/tests/article.test.js
+++ commands/tests/article.test.js
@@ -1,5 +1,40 @@
 const article = require('../article.js');
+const Parser = require('rss-parser');
 const messageStub = require('../../stub/messageStub.js');
+
+jest.mock('rss-parser');
+
+beforeAll(() => {
+	jest.spyOn(Parser.prototype, 'parseURL').mockImplementation((link) => {
+		if(link == 'https://knightsofacademia.org/category/articles/feed')
+		{
+			return {
+				items: [
+					{
+						creator: 'Amy KOA',
+						title: 'Banish Procrastination With The Five Second Rule.',
+						link: 'https://knightsofacademia.org/banish-procrastination-with-the-five-second-rule/?utm_source=rss&utm_medium=rss&utm_campaign=banish-procrastination-with-the-five-second-rule',
+					},
+					{
+						creator: 'Amy KOA',
+						title: 'How Do I Start Meditating',
+						link: 'https://knightsofacademia.org/how-do-i-start-meditating/?utm_source=rss&utm_medium=rss&utm_campaign=how-do-i-start-meditating',
+					},
+					{
+						creator: 'Knights of Academia',
+						title: 'Knowledge is Power, Execution is Royalty (and Perception is Noteworthy)',
+						link: 'https://knightsofacademia.org/knowledge-is-power/?utm_source=rss&utm_medium=rss&utm_campaign=knowledge-is-power',
+					}
+				],
+				feedUrl: 'https://knightsofacademia.org/category/articles/feed/'
+			};
+		}
+	});
+});
+
+afterAll(() => {
+	jest.restoreAllMocks();
+});
 
 test('Sends correct response with no arguments', async() => {
 	const message = new messageStub();
--- commands/tests/help.test.js
+++ commands/tests/help.test.js
@@ -1,61 +1,59 @@
 const Discord = require('discord.js');
 const help = require('../help.js');
 
-describe('Help Command', () => {
-	const client = new Discord.Client();
-	const message = ({
-		channel: {
-			send: jest.fn((msg) => Promise.resolve(msg)),
-		},
-		author: {
-			send: jest.fn((msg) => Promise.resolve(msg)),
-		},
-		guild: {
-			name: 'KOA'
-		}
-	});
+const client = new Discord.Client();
+const message = ({
+	channel: {
+		send: jest.fn((msg) => Promise.resolve(msg)),
+	},
+	author: {
+		send: jest.fn((msg) => Promise.resolve(msg)),
+	},
+	guild: {
+		name: 'KOA'
+	}
+});
 
-	beforeAll(() => {
-		client.commands = new Discord.Collection();
-		client.aliases = new Discord.Collection();
-		['help', 'facebook','invite','coinflip'].forEach(name => 
-			client.commands.set(name, require(`../${name}`))
-		);
-	});
+beforeAll(() => {
+	client.commands = new Discord.Collection();
+	client.aliases = new Discord.Collection();
+	['help', 'facebook','invite','coinflip'].forEach(name => 
+		client.commands.set(name, require(`../${name}`))
+	);
+});
 
-	beforeEach(() => {
-		jest.clearAllMocks();
-	});
+beforeEach(() => {
+	jest.clearAllMocks();
+});
 
-	afterAll(() => {
-		client.destroy();
-	});
+afterAll(() => {
+	client.destroy();
+});
 
-	it('sends default help message when no argument is provided', async () => {	
-		await help.execute(client, message, []);
+test('Sends default help message when no argument is provided', async () => {	
+	await help.execute(client, message, []);
+
+	const expectedResponse = 'I have sent you a private message with the command list.';
+	expect(message.author.send).toHaveBeenCalledTimes(1);
+	expect(message.channel.send).toHaveBeenCalledWith(expectedResponse);
+});
+
+test('Sends default help message when an argument is provided', async () => {
+	await help.execute(client, message, ['help']);
+
+	const expectedResponse = new Discord.MessageEmbed()
+		.setColor('#ff0000')
+		.setTitle('!help')
+		.setDescription('You asked for information on !help')
+		.addField('Description:', 'I will send you this message, or the usage of a specific command.')
+		.addField('Aliases:', 'help')
+		.addField('Usage:', 'help\nhelp command');
+	expect(message.channel.send).toHaveBeenCalledWith(expectedResponse);
+});
+
+test('Sends name correction when an incorrect command is looked up', async () => {
+	await help.execute(client, message, ['yelp']);
 	
-		const expectedResponse = 'I have sent you a private message with the command list.';
-		expect(message.author.send).toHaveBeenCalledTimes(1);
-		expect(message.channel.send).toHaveBeenCalledWith(expectedResponse);
-	});
-
-	it('sends default help message when an argument is provided', async () => {
-		await help.execute(client, message, ['help']);
-
-		const expectedResponse = new Discord.MessageEmbed()
-			.setColor('#ff0000')
-			.setTitle('!help')
-			.setDescription('You asked for information on !help')
-			.addField('Description:', 'I will send you this message, or the usage of a specific command.')
-			.addField('Aliases:', 'help')
-			.addField('Usage:', 'help\nhelp command');
-		expect(message.channel.send).toHaveBeenCalledWith(expectedResponse);
-	});
-
-	it('sends name correction when an incorrect command is looked up', async () => {
-		await help.execute(client, message, ['yelp']);
-		
-		const expectedResponse = 'Did you mean `!help help`?';
-		expect(message.channel.send).toHaveBeenCalledWith(expectedResponse);
-	});
+	const expectedResponse = 'Did you mean `!help help`?';
+	expect(message.channel.send).toHaveBeenCalledWith(expectedResponse);
 });
