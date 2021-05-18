405b4eee3bb1b51a75b3b15d68e50e9f97379711
Debashis Biswas <debashis.biswas@asu.edu>
2021-05-17 17:17:28 -0700

    finished accountability unpin tests

--- commands/tests/accountabilityPins.test.js
+++ commands/tests/accountabilityPins.test.js
@@ -6,66 +6,70 @@ beforeEach(async () => {
 	jest.resetModules().resetAllMocks();
 });
 
-test('"Unpin" calls handler when in accountability channel', async () => {
-	jest.mock('../../config.json', () => {
-		return {
-			channels: {
-				accountability: '123123123123123123'
-			}
-		};
-	});
-	jest.mock('../../eventActions/accountabilityActions.js');
-	const accActions = require('../../eventActions/accountabilityActions.js');
+describe('Accountability when inside correct channel', () => {
+	test('"Unpin" calls handler when in accountability channel', async () => {
+		jest.mock('../../config.json', () => {
+			return {
+				channels: {
+					accountability: '123123123123123123'
+				}
+			};
+		});
+		jest.mock('../../eventActions/accountabilityActions.js');
+		const accActions = require('../../eventActions/accountabilityActions.js');
 
-	const unpin = require('../accountabilityUnpin.js');
-	await unpin.execute(null, message);
-	expect(accActions.userUnpinsMessage).toBeCalledTimes(1);
+		const unpin = require('../accountabilityUnpin.js');
+		await unpin.execute(null, message);
+		expect(accActions.userUnpinsMessage).toBeCalledTimes(1);
+	});
+
+	test('"Unpin All" calls handler when in accountability channel', async () => {
+		jest.mock('../../config.json', () => {
+			return {
+				channels: {
+					accountability: '123123123123123123'
+				}
+			};
+		});
+		jest.mock('../../eventActions/accountabilityActions.js');
+		const accActions = require('../../eventActions/accountabilityActions.js');
+
+		const unpinAll = require('../accountabilityUnpinAll.js');
+		await unpinAll.execute(null, message);
+		expect(accActions.userUnpinsAllMessages).toBeCalledTimes(1);
+	});
 });
 
-test('"Unpin" does not call handler when not in accountability channel', async () => {
-	jest.mock('../../config.json', () => {
-		return {
-			channels: {
-				accountability: '000'
-			}
-		};
+describe('Accountability when outside correct channel', () => {
+	test('"Unpin" does not call handler when not in accountability channel', async () => {
+		jest.mock('../../config.json', () => {
+			return {
+				channels: {
+					accountability: '000'
+				}
+			};
+		});
+		jest.mock('../../eventActions/accountabilityActions.js');
+		const accActions = require('../../eventActions/accountabilityActions.js');
+
+		const unpin = require('../accountabilityUnpin.js');
+		await unpin.execute(null, message);
+		expect(accActions.userUnpinsMessage).not.toBeCalled();
 	});
-	jest.mock('../../eventActions/accountabilityActions.js');
-	const accActions = require('../../eventActions/accountabilityActions.js');
 
-	const unpin = require('../accountabilityUnpin.js');
-	await unpin.execute(null, message);
-	expect(accActions.userUnpinsMessage).not.toBeCalled();
-});
+	test('"Unpin All" does not call handler when not in accountability channel', async () => {
+		jest.mock('../../config.json', () => {
+			return {
+				channels: {
+					accountability: '000'
+				}
+			};
+		});
+		jest.mock('../../eventActions/accountabilityActions.js');
+		const accActions = require('../../eventActions/accountabilityActions.js');
 
-test('"Unpin All" calls handler when in accountability channel', async () => {
-	jest.mock('../../config.json', () => {
-		return {
-			channels: {
-				accountability: '123123123123123123'
-			}
-		};
+		const unpinAll = require('../accountabilityUnpinAll.js');
+		await unpinAll.execute(null, message);
+		expect(accActions.userUnpinsAllMessages).not.toBeCalled();
 	});
-	jest.mock('../../eventActions/accountabilityActions.js');
-	const accActions = require('../../eventActions/accountabilityActions.js');
-
-	const unpinAll = require('../accountabilityUnpinAll.js');
-	await unpinAll.execute(null, message);
-	expect(accActions.userUnpinsAllMessages).toBeCalledTimes(1);
-});
-
-test('"Unpin All" does not call handler when not in accountability channel', async () => {
-	jest.mock('../../config.json', () => {
-		return {
-			channels: {
-				accountability: '000'
-			}
-		};
-	});
-	jest.mock('../../eventActions/accountabilityActions.js');
-	const accActions = require('../../eventActions/accountabilityActions.js');
-
-	const unpinAll = require('../accountabilityUnpinAll.js');
-	await unpinAll.execute(null, message);
-	expect(accActions.userUnpinsAllMessages).not.toBeCalled();
 });
