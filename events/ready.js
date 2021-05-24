fcffd0620699fe6e118c397a19559e618feb0292
Debashis Biswas <debashis.biswas@asu.edu>
2021-05-24 12:01:06 -0700

    updated exentions in ready.js, added awaits

--- events/ready.js
+++ events/ready.js
@@ -1,15 +1,15 @@
-const chainMessageAction = require('../eventActions/checkChainMessage');
+const chainMessageAction = require('../eventActions/checkChainMessage.js');
 const config = require('../config.json');
-const { sequelize } = require('../databaseFiles/connect');
-const remind = require('../commands/remind');
+const { sequelize } = require('../databaseFiles/connect.js');
+const remind = require('../commands/remind.js');
 const tosRemind = require('../eventActions/tosReminderAction.js');
 
 module.exports = async (client) => {
 	await sequelize.sync();
-	chainMessageAction.loadChainMessageTracker(client);
-	remind.catchUp(client);
+	await chainMessageAction.loadChainMessageTracker(client);
+	await remind.catchUp(client);
 	setInterval(remind.scanForReminders, config.reminderScanInterval, client);
 	setInterval(tosRemind.tosRemind, config.tosReminderTimer, client);
-	client.user.setActivity('Ready to !help');
+	await client.user.setActivity('Ready to !help');
 	console.log(`Running on ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers.`);
 };
