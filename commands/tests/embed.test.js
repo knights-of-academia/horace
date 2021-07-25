83f6988581fca9d239c8f7a0c151fce90efe4ee5
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2021-07-24 20:01:14 -0700

    WIP: started on embed command tests

---
+++ commands/tests/embed.test.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const embed = require('../embed.js');
+const MockMessage = require('../../stub/MockMessage.js');
+
+test('Validates channel correctly given correct input', async () => {
+	const expectedResponse
+
+});
+
