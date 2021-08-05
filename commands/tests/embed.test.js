cc4cef93eb5ae8cf6cfbbe1ca9039572fb7f6327
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2021-08-04 22:56:17 -0700

    Finally figured out how Jest works

--- commands/tests/embed.test.js
+++ commands/tests/embed.test.js
@@ -2,8 +2,38 @@ const Discord = require('discord.js');
 const embed = require('../embed.js');
 const MockMessage = require('../../stub/MockMessage.js');
 
-test('Validates channel correctly given correct input', async () => {
-	const expectedResponse
-
+beforeEach(async () => {
+	jest.resetModules().resetAllMocks();
 });
 
+test('Validates title correctly given correct input', async () => {
+	jest.spyOn(embed, 'ask').mockImplementation(jest.fn());
+	embed.ask.mockReturnValue('testTitle');
+	const expectedResponse = 'testTitle';
+	const responseMessage = await embed.getTitle();
+	expect(responseMessage).toBe(expectedResponse);
+});
+
+test('Validates title correctly given incorrect input', async () => {
+	jest.spyOn(embed, 'ask').mockImplementation(jest.fn());
+	embed.ask.mockReturnValue(null);
+	await expect(embed.getTitle())
+		.rejects
+		.toThrow('Embed title not valid!');
+});
+
+test('Validates URL correctly given correct input', async () => {
+	jest.spyOn(embed, 'ask').mockImplementation(jest.fn());
+	embed.ask.mockReturnValue('https://example.com');
+	const expectedResponse = 'https://example.com';
+	const responseMessage = await embed.getURL();
+	expect(responseMessage).toBe(expectedResponse);
+});
+
+test('Validates URL correctly given incorrect input', async () => {
+	jest.spyOn(embed, 'ask').mockImplementation(jest.fn());
+	embed.ask.mockReturnValue(null);
+	await expect(embed.getURL())
+		.rejects
+		.toThrow('Embed URL not valid!');
+});
