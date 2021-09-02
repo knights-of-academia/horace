640d0b7d6744415a6357ed14e828240675cdc0f2
SaiAishu <30656747+SaiAishu@users.noreply.github.com>
2021-09-02 22:18:32 +0530

    Ticket#187
    Added Staff application links to !apply and list of clans and positions in error message.

---
+++ data/staffteam-data.js
@@ -0,0 +1,26 @@
+module.exports = [
+	{
+		formUrl: 'https://knightsofacademia.org/clan-leader-application/',
+		names: ['clan leader','clan leaders', 'clanleader', 'clanleaders']
+	},
+	{
+		formUrl: 'https://knightsofacademia.org/sector-leader-application/',
+		names: ['sector leader','sector leaders', 'sectorleader', 'sectorleaders']
+	},
+	{
+		formUrl: 'https://knightsofacademia.org/web-producer-application/',
+		names: ['web producer','web producers','webproducer','webproducers','web developer', 'website team']
+	},
+	{
+		formUrl: 'https://knightsofacademia.org/engineering-app/',
+		names: ['engineer','engineers', 'engineering', 'engineering team','engineeringteam']
+	},
+	{
+		formUrl: 'https://knightsofacademia.org/content-creator-application/',
+		names: ['content creator','content creators', 'contentcreator','contentcreators','content crafter', 'content writer']
+	},
+	{
+		formUrl: 'https://knightsofacademia.org/koai-keeper-application/',
+		names: ['koai keeper','koai keepers','koaikeeper','koaikeepers' 'keeper']
+	}
+];
