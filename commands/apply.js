0cf2c8fb6e3fbbee9e608643ab4bfc05903b7424
Danny.Fox <30656747+SaiAishu@users.noreply.github.com>
2021-09-07 00:26:35 +0530

    Ticket#187 (#278)
    * Ticket#187
    
    Added Staff application links to !apply and list of clans and positions in error message.
    
    * Code cleaned
    
    * Linter ran
    
    * Linter changes for apply.js
    
    * Linter run 3

--- commands/apply.js
+++ commands/apply.js
@@ -1,32 +1,56 @@
 const clans = require('../data/clan-data');
+const positions = require('../data/staffteam-data');
 
 module.exports.execute = async (client, message, args) => {
 	const searchTerm = args.join(' ').toLowerCase();
 
 	if (searchTerm.length === 0) {
 		return await message.channel.send(
-			'❌ Please specify the clan you wish to apply for.'
+			'❌ Please specify the clan or staff position you wish to apply for.'
 		);
 	}
 
 	let clan = clans.find((c) => c.names.includes(searchTerm));
-
-	if (!clan) {
+	let position = positions.find((p) => p.names.includes(searchTerm));
+	if (!position && !clan) {
 		return await message.channel.send(
-			`❌ The clan \`${searchTerm}\` couldn't be found.`
+			`❌ The clan or staff position \`${searchTerm}\` couldn't be found.
+			
+			Here is our list of KOA Clans!
+				🔸 **The Round Table**: All things Hard Mode
+				🔸 **The Fiction Faction**: Creative Writing & Story Telling 
+				🔸 **The Gathering**: Accountability 
+				🔸 **The Clockwork Knights**: Productivity & Efficiency through the use of Systems
+				🔸 **The Silver Tongues**: Language & Culture
+				🔸 **The Students**: Academics & all things Education
+				🔸 **The Wolf Pack**: On the move for Health
+				
+			Here is our list of KOA Staff Positions!
+				🔸 **Clan Leaders** 
+				🔸 **Sector Leaders** 
+				🔸 **Web Producers**
+				🔸 **Engineers**
+				🔸 **Content Crafters**
+				🔸 **KOAI Keepers**`
 		);
 	}
 
-	const response = `✔ **Fill out your user ID to receive an invite!**
+	let response = null;
+	if (clan) {
+		response = `✔ **Fill out your user ID to receive an invite!**
 *Average Response Time: 24 hours or less*
 ${clan.formUrl}`;
-
+	} else if (position) {
+		response = `✔ **Fill out your user ID to receive an invite!**
+*Average Response Time: 24 hours or less*
+${position.formUrl}`;
+	}
 	return await message.channel.send(response);
 };
 
 module.exports.config = {
 	name: 'apply',
 	aliases: ['apply', 'applyto'],
-	description: 'I will send you an application form for the clan you wish to join.',
-	usage: ['apply clan']
+	description: 'I will send you an application form for the clan or staff position you wish to join.',
+	usage: ['apply clan', 'apply position']
 };
