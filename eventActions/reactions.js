8a0324e48a6c6227efad801a7adf04f6319b65a0
Debashis Biswas <debashis.biswas@asu.edu>
2021-09-09 15:10:32 -0700

    change argument ordering

--- eventActions/reactions.js
+++ eventActions/reactions.js
@@ -1,12 +1,13 @@
 const { Config } = require('../config');
 
-// TODO: order should be channel, emote - switch these two
 const react = async function(
 	message,
+	idWhitelist,
 	emoteToAdd,
-	idWhitelist = [],
 	regex = new RegExp()
 ) {
+	if (!idWhitelist) idWhitelist = [];
+
 	const conditions = [
 		idWhitelist.includes(message.channel.id),
 		regex.test(message)
@@ -23,32 +24,32 @@ const react = async function(
 
 const handleReactions = async function(message, isCommand) {
 	if (isCommand) return;
-
+	// TODO: make this function prettier
 	// TODO: run these together. make a convenience function for this promise pattern
 	// TODO: avoid repeated code: build an object? some functional programming?
-	await react(message, Config.EMOTES.HOC_REACTION, Config.CHANNELS.HALL_OF_CONQUESTS);
-	await react(message, Config.EMOTES.HEART, Config.CHANNELS.CONTENT_NOTIFIER);
-	await react(message, Config.EMOTES.GRATITUDE, Config.CHANNELS.GRATITUDE, new RegExp('today +i +am +grateful +for', 'i'));
-	await react(message, Config.EMOTES.SLEEP_LOG_REACTION, Config.CHANNELS.SLEEP_CLUB, new RegExp('sleep log', 'gi'));
-	await react(message, ['🎄', '☃️', '❄️'], Config.CHANNELS.CITADEL, new RegExp('merry|christmas', 'gi'));
-	await react(message, Config.EMOTES.COTW_VOW, Config.CHANNELS.COTW, new RegExp('i vow to', 'gi'));
-	await react(message, Config.EMOTES.POM, Config.CHANNELS.ACCOUNTABILITY, new RegExp(' pom', 'gi'));
+	await react(message, Config.CHANNELS.HALL_OF_CONQUESTS, Config.EMOTES.HOC_REACTION);
+	await react(message, Config.CHANNELS.CONTENT_NOTIFIER, Config.EMOTES.HEART);
+	await react(message, Config.CHANNELS.GRATITUDE, Config.EMOTES.GRATITUDE, new RegExp('today +i +am +grateful +for', 'i'));
+	await react(message, Config.CHANNELS.SLEEP_CLUB, Config.EMOTES.SLEEP_LOG_REACTION, new RegExp('sleep log', 'gi'));
+	await react(message, Config.CHANNELS.COTW, Config.EMOTES.COTW_VOW, new RegExp('i vow to', 'gi'));
+	await react(message, Config.CHANNELS.CITADEL, ['🎄', '☃️', '❄️'], new RegExp('merry|christmas', 'gi'));
 
-	await react(message, '🇫🇷', Config.CHANNELS.ACCOUNTABILITY, new RegExp('french', 'gi'));
-	await react(message, '🇪🇸', Config.CHANNELS.ACCOUNTABILITY, new RegExp('spanish', 'gi'));
-	await react(message, '🇮🇹', Config.CHANNELS.ACCOUNTABILITY, new RegExp('italian', 'gi'));
+	await react(message, Config.CHANNELS.ACCOUNTABILITY, Config.EMOTES.POM, new RegExp(' pom', 'gi'));
+	await react(message, Config.CHANNELS.ACCOUNTABILITY, '🇫🇷', new RegExp('french', 'gi'));
+	await react(message, Config.CHANNELS.ACCOUNTABILITY, '🇪🇸', new RegExp('spanish', 'gi'));
+	await react(message, Config.CHANNELS.ACCOUNTABILITY, '🇮🇹', new RegExp('italian', 'gi'));
 
 	await react(
 		message,
-		Config.EMOTES.ACCOUNTABILITY_EMOTES_ARRAY,
 		Config.CHANNELS.ACCOUNTABILITY,
+		Config.EMOTES.ACCOUNTABILITY_EMOTES_ARRAY,
 		new RegExp(`${Config.EMOTES.YES2}|:✅:`, 'gi')
 	);
 
 	await react(
 		message,
-		Config.EMOTES.COTW_REFLECTION,
 		Config.CHANNELS.COTW,
+		Config.EMOTES.COTW_REFLECTION,
 		// Match 0 - n instances of a word followed by space, followed by "reflection".
 		// This tests if "reflection" is present in the first n words of the string.
 		new RegExp(`^(\\w+\\s+){0,${Config.REFLECTION_CHECK_DEPTH - 1}}(reflection)`, 'i')
@@ -56,8 +57,8 @@ const handleReactions = async function(message, isCommand) {
 
 	await react(
 		message,
-		Config.EMOTES.GOOD_MORNING,
 		Config.CHANNELS.CITADEL,
+		Config.EMOTES.GOOD_MORNING,
 		Config.STRICT_GREETINGS
 			? new RegExp('mo*rning (koa[^a-z]*|knights[^a-z]*|friends[^a-z]*|everyone[^a-z]*)$')
 			: new RegExp('g+o{2,}d+\\s*m+o+r+n+i+n+g+')
@@ -65,8 +66,8 @@ const handleReactions = async function(message, isCommand) {
 
 	await react(
 		message,
-		Config.EMOTES.GOOD_NIGHT,
 		Config.CHANNELS.CITADEL,
+		Config.EMOTES.GOOD_NIGHT,
 		Config.STRICT_GREETINGS
 			? new RegExp('ni*ght (koa[^a-z]*|knights[^a-z]*|friends[^a-z]*|everyone[^a-z]*)$')
 			: new RegExp('g+o{2,}d+\\s*n+i+g+h+t+')
