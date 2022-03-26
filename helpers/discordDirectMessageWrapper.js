3c17c84aed9f7a84d615e6023af6416b30d7a297
Catalin Popescu <catalinp86@gmail.com>
2022-03-26 10:02:05 -0400

    Advises users how to enable DMs back

--- helpers/discordDirectMessageWrapper.js
+++ helpers/discordDirectMessageWrapper.js
@@ -20,5 +20,6 @@ module.exports.sendMessage = async (user, message) => {
 };
 
 module.exports.sendBlockedDMsWarning = async (channel, reason) => {
-	await channel.send(`I tried to let you know ${reason}. \nI was not able to send you a message, because your DMs were disabled`);
+	await channel.send(`I tried to let you know ${reason}. \nI was not able to send you a message, because your DMs were disabled. `);
+	await channel.send('Here\'s how to allow me to direct message you!\nhttps://support.discord.com/hc/en-us/articles/217916488-Blocking-Privacy-Settings-\nInstead of blocking my messages, be sure to toggle this setting off to allow messages.');
 };
