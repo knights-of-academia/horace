22a7df4779c1d6a4d5028f122e50f7f258c2cfc0
CJ <86695177+cjtheham@users.noreply.github.com>
2022-03-26 21:55:22 -0500

    Updated clans.js with new leader info (#284)

--- commands/clans.js
+++ commands/clans.js
@@ -4,10 +4,10 @@ module.exports.execute = async (client, message) => {
 	🔸 **The Round Table**: All things Hard Mode by **bendre1997**#9332
 	🔸 **The Fiction Faction**: Creative Writing & Story Telling by **varrictethras**#5383
 	🔸 **The Gathering**: Accountability by **hikikomori**#3771
-	🔸 **The Clockwork Knights**: Productivity & Efficiency through the use of Systems by **stoneybaby**#3398
-	🔸 **The Silver Tongues**: Language & Culture by **theamazingsplit**#2229
-	🔸 **The Students**: Academics & all things Education by **colin**#3523
-	🔸 **The Wolf Pack**: On the move for Health by **QueenWolf**#5509`);
+	🔸 **The Clockwork Knights**: Productivity & Efficiency through the use of Systems by **stoneybaby**#3398 - *looking for new clan leader!*
+	🔸 **The Silver Tongues**: Language & Culture by **MonKhan**#1913
+	🔸 **The Students**: Academics & all things Education by **ridgel0rd0**#7176
+	🔸 **The Wolf Pack**: On the move for Health by **Sergeant Steadfast**#1832`);
 };
 
 module.exports.config = {
