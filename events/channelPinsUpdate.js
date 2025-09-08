a9645f8e5615c0f97b7e85fd362465cd2a840d6e
CatalinCodes <12548925+catalincodes@users.noreply.github.com>
2025-09-08 19:00:25 -0400

    Temporary, but useful for testing

--- events/channelPinsUpdate.js
+++ events/channelPinsUpdate.js
@@ -1,12 +1,14 @@
 module.exports = async (client, channel) => {
 	const currentChannel = client.channels.cache.get(channel.id);
+	//TODO: make sure to put back the original 50 limit and 45 warning
+	//TODO: deprecated and to be replaced with fetchPins in v14
 	currentChannel.messages.fetchPinned().then((messages) => {
 		const numOfPins = messages.size;
-		if (numOfPins === 50) {
+		if (numOfPins === 5) {
 			currentChannel.send('**Uh oh!** This channel has reached its pin limit. Contact a Helper to purge the list.');
 		}
-		else if (numOfPins >= 45) {
+		else if (numOfPins >= 3) {
 			currentChannel.send('**Heads up!** This channel is almost at its pin limit! There are currently ' + numOfPins + ' pinned messages.');
 		}
-	});
+	}).catch(console.error);
 };
